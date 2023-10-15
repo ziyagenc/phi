9319775ec49baccd4c54c1204df6979d16ecacce
Ziya Genc <ziya.genc@datasecure.ai>
2023-10-15 19:48:22 +0200

    Use console.log() for logging.

--- extension.js
+++ extension.js
@@ -5,13 +5,13 @@ const { PiholeIndicator } = Me.imports.indicator;
 
 class PiholeExtension {
   enable() {
-    log(`Enabling ${Me.metadata.name}.`);
+    console.log(`Enabling ${Me.metadata.name}.`);
 
     this._indicator = new PiholeIndicator();
   }
 
   disable() {
-    log(`Disabling ${Me.metadata.name}.`);
+    console.log(`Disabling ${Me.metadata.name}.`);
 
     if (this._indicator) {
       this._indicator.destroy();
