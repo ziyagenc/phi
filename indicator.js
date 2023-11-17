0dd33103597f9aa5ef324067383d7156cb5418d5
Sankalp <mail@sghatpande.eu>
2023-11-17 15:16:41 +0100

    [Add] Docstring for indicator.js

--- indicator.js
+++ indicator.js
@@ -1,15 +1,23 @@
-import Gio from "gi://Gio";
-import GObject from "gi://GObject";
-
-import * as DBus from "./dbus.js";
-import { Pihole as SingleInstance } from "./pihole.js";
-import { MPihole as MultiInstance } from "./mpihole.js";
-
+/**
+ * A class that manages the Pihole indicator based on the 
+ * user's settings and the availability of the network
+ *
+ * @class PiholeIndicator
+ */
 export const PiholeIndicator = GObject.registerClass(
   {
     GTypeName: "PiholeIndicator",
   },
   class PiholeIndicator extends GObject.Object {
+
+    /**
+     * Creates an instance of PiholeIndicator
+     *
+     * @param {Object} me - The current instance
+     * @param {Object} settings - The user's current settings
+     * @memberof PiholeIndicator
+     */
+
     constructor(me, settings) {
       super();
 
@@ -23,12 +31,24 @@ export const PiholeIndicator = GObject.registerClass(
       this._start().catch(console.error);
     }
 
+    /**
+     * Retrieves the user's settings
+     *
+     * @memberof PiholeIndicator
+     */
+
     _configure() {
       this._hideUi = this._settings.get_boolean("hideui");
       this._restrict = this._settings.get_boolean("restrict");
       this._networks = this._settings.get_string("networks");
       this._multimode = this._settings.get_boolean("multimode");
     }
+
+    /**
+     * Connects event handlers for network changes and settings changes
+     *
+     * @memberof PiholeIndicator
+     */
 
     _setHandlers() {
       this._networkHandlerId = this._network_monitor.connect(
@@ -43,6 +63,13 @@ export const PiholeIndicator = GObject.registerClass(
         this._start().catch(console.error);
       });
     }
+
+    /**
+     * Determines the appropriate Pihole instance to create based on the 
+     * network availability and the user's settings
+     *
+     * @memberof PiholeIndicator
+     */
 
     async _start() {
       const Pihole = this._multimode ? MultiInstance : SingleInstance;
@@ -71,6 +98,12 @@ export const PiholeIndicator = GObject.registerClass(
           this._pihole = new Pihole(this._me, this._settings, "no_network");
       }
     }
+
+    /**
+     * Disconnects event handlers and destroys the instance
+     *
+     * @memberof PiholeIndicator
+     */
 
     destroy() {
       this._settings.disconnect(this._settingsHandlerId);
